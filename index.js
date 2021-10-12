71c599ac0f5d9b1c52b912e02e0bde6c91f04b72
Yaritza <yaritzamicaelleb@gmail.com>
2021-10-12 12:46:43 -0300

    js nmcompleto

---
+++ index.js
@@ -0,0 +1,8 @@
+let nome= prompt("Digite seu nome:") || "";
+let sobrenome= prompt("Digite seu sobrenome:")||"";
+
+let nomecompleto= `Nome completo: ${nome} ${sobrenome}`;
+let nomecatalogo= `Nome de catálogo: ${sobrenome.toUpperCase()}, ${nome}`;
+
+alert(nomecompleto);
+alert(nomecatalogo);
